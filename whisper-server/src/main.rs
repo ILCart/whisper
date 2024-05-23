fddf672627f21078651f595d5ecc273472134f5d
ILCart <isaac.lay23@student.cart.org>
2024-05-23 10:43:56 -0700

    websocket server working

--- whisper-server/src/main.rs
+++ whisper-server/src/main.rs
@@ -1,33 +1,51 @@
-mod events;
+// mod events;
 extern crate env_logger;
 extern crate ws;
 
-use ws::{listen, CloseCode, Handler, Handshake, Message, Request, Response, Result, Sender};
+use std::collections::HashSet;
+
+use serde::{Deserialize, Serialize};
+use serde_json::{self, json, Value};
 use uuid::Uuid;
-use serde_json;
-use serde::{Serialize, Deserialize};
+use ws::{
+    listen, CloseCode, Handler, Handshake, Message, Request, Response, Result as ws_result, Sender,
+};
 
 struct Server {
     out: Sender,
-    clients: Vec<Client>,
+    clients: HashSet<Client>,
     created_ids: Vec<Uuid>,
 }
-
-struct Client{
+#[derive(Eq, Hash, PartialEq)]
+struct Client {
     uuid: Uuid,
+    username: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct ClientMessage {
+    data: Value,
+    uuid: String,
+}
 
-
-
-impl Server{
-    fn create_uuid(&mut self) -> Uuid{
+impl Server {
+    fn create_uuid(&mut self) -> Uuid {
         let mut new_uuid = Uuid::new_v4();
         while self.created_ids.contains(&new_uuid) {
             new_uuid = Uuid::new_v4();
         }
         self.created_ids.push(new_uuid);
         new_uuid
+    }
+    fn uuid_is_client(&self, uuid: Uuid) -> bool {
+        let client_index = self.clients.iter().find(|x| x.uuid == uuid);
+        match client_index {
+            Some(client) => self.clients.contains(client),
+            None => false
+        }
+    }
+    fn get_client_from_uuid(&self,s: Uuid) -> &Client {
+        self.clients.iter().find(|p| p.uuid == s).expect("Expected Valid UUID (eval before this)")
     }
     // fn is_uuid_dead(&self,needle: &Uuid) -> bool{
     //     self.dead_ids.contains(needle)
@@ -39,38 +57,81 @@ impl Server{
     //     }
     // }
 }
-impl Handler for Server{
-    fn on_request(&mut self, req: &Request) -> Result<Response> {
+
+fn is_uuid(s: &str) -> Result<Uuid, String> {
+    match Uuid::parse_str(s) {
+        Ok(f) => Ok(f),
+        Err(_e) => Err("NOT A UUID".to_string()),
+    }
+}
+
+// fn match_event(message:Value) {
+//     return message.as_array();
+// }
+
+impl Handler for Server {
+    fn on_request(&mut self, req: &Request) -> ws_result<Response> {
         match req.resource() {
             "/ws" => Response::from_request(req),
             "/" => Ok(Response::new(200, "OK", b"<p>Gyat</p>".to_vec())),
             _ => Ok(Response::new(404, "Not Found", b"404 - Not Found".to_vec())),
         }
     }
-    
-    fn on_open(&mut self, shake: Handshake) -> Result<()> {
+
+    fn on_open(&mut self, shake: Handshake) -> ws_result<()> {
         let id: Uuid = self.create_uuid();
-        let connected_client: Client = Client {uuid:id};
-        self.clients.push(connected_client);
-        println!("Connection opened to client: {}, Their UUID is : {}",shake.peer_addr.unwrap(),id);
-        self.out.send(id.to_string())
+        let connected_client: Client = Client {
+            uuid: id,
+            username: "Test".to_owned(),
+        };
+        self.clients.insert(connected_client);
+        println!(
+            "Connection opened to client: {}, Their UUID is : {}",
+            shake.peer_addr.unwrap(),
+            id
+        );
+        let send_back = json!({"uuid":id.to_string()});
+        self.out.send(send_back.to_string())
     }
     fn on_close(&mut self, code: CloseCode, reason: &str) {
-        println!("Connection closed to client. Closed with code: '{0:#?}' Reason : '{1}'",code,reason);
+        println!(
+            "Connection closed to client. Closed with code: '{0:#?}' Reason : '{1}'",
+            code, reason
+        );
     }
-    fn on_message(&mut self, msg: Message) -> Result<()> {
-        println!("Client sent message: {:#?}",msg);
-        self.out.send("received")
+    fn on_message(&mut self, msg: Message) -> ws_result<()> {
+        println!("Client sent message: {:#?}", msg);
+        let message: ClientMessage = serde_json::from_str(msg.as_text().expect("Expected text"))
+            .expect("Expected 'uuid' and 'data'");
+  
+        let parsed_uuid = is_uuid(&message.uuid) ;
+        match parsed_uuid {
+            Ok(uuid) => {
+                if self.uuid_is_client(uuid){
+                    let client = self.get_client_from_uuid(uuid);
+                    // match_event(message.data);
+                    println!("{} ({}): {}", client.username, client.uuid, message.data);
+                }
+                
+                self.out.send("received")
+            }
+            Err(msg) => {
+                eprintln!("{msg} ({})", &message.uuid);
+                self.out.send("invaliud auth")
+            }
+        }
     }
 }
 
-
-
-fn main(){
+fn main() {
     env_logger::init();
     let port = "3001";
     let mut addr = "127.0.0.1:".to_owned();
     addr.push_str(port);
-    listen(addr, |out| Server {out, clients: Vec::new(), created_ids: Vec::new() }).unwrap();
+    listen(addr, |out| Server {
+        out,
+        clients: HashSet::new(),
+        created_ids: Vec::new(),
+    })
+    .unwrap();
 }
-
--- whisper-server/target/debug/deps/percent_encoding-28139051d83695e1.d
+++ whisper-server/target/debug/deps/percent_encoding-28139051d83695e1.d
@@ -1,5 +1,5 @@
-C:\Users\150075889\Documents\GitHub\whisper\whisper-server\target\debug\deps\libpercent_encoding-28139051d83695e1.rmeta: C:\Users\150075889\.cargo\registry\src\index.crates.io-6f17d22bba15001f\percent-encoding-2.3.1\src\lib.rs
+C:\Users\150073110\Documents\GitHub\whisper\whisper-server\target\debug\deps\libpercent_encoding-28139051d83695e1.rmeta: C:\Users\150073110\.cargo\registry\src\index.crates.io-6f17d22bba15001f\percent-encoding-2.3.1\src\lib.rs
 
-C:\Users\150075889\Documents\GitHub\whisper\whisper-server\target\debug\deps\percent_encoding-28139051d83695e1.d: C:\Users\150075889\.cargo\registry\src\index.crates.io-6f17d22bba15001f\percent-encoding-2.3.1\src\lib.rs
+C:\Users\150073110\Documents\GitHub\whisper\whisper-server\target\debug\deps\percent_encoding-28139051d83695e1.d: C:\Users\150073110\.cargo\registry\src\index.crates.io-6f17d22bba15001f\percent-encoding-2.3.1\src\lib.rs
 
-C:\Users\150075889\.cargo\registry\src\index.crates.io-6f17d22bba15001f\percent-encoding-2.3.1\src\lib.rs:
+C:\Users\150073110\.cargo\registry\src\index.crates.io-6f17d22bba15001f\percent-encoding-2.3.1\src\lib.rs:
