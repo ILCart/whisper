f0ddc5ea7dde09ee52e66e9acebc6d6de1b5476a
K6id3n <143832452+K6id3n@users.noreply.github.com>
2024-05-23 10:45:15 -0700

    history almost done

--- ui.js
+++ ui.js
@@ -1,67 +1,92 @@
-window.onload = function() {
+// window.onload = function() {
 
-    // Get references to elements on the page.
-    var form = document.getElementById('message-form');
-    var messageField = document.getElementById('message');
-    var messagesList = document.getElementById('messages');
-    var socketStatus = document.getElementById('status');
-    var closeBtn = document.getElementById('close');
-  
-  
-    // Create a new WebSocket.
-    var socket = new WebSocket('ws://127.0.0.1:3001/ws');
-  
-  
-    // Handle any errors that occur.
-    socket.onerror = function(error) {
-      console.log('WebSocket Error: ' + error);
-    };
-  
-  
-    // Show a connected message when the WebSocket is opened.
-    socket.onopen = function(event) {
-        console.log('open');
-    };
-  
-  
-    // Handle messages sent by the server.
-    socket.onmessage = function(event) {
-        console.log(event.data);
-    };
-  
-  
-    // Show a disconnected message when the WebSocket is closed.
-    socket.onclose = function(event) {
-        console.log('open');
-    };
-  
-  
-    // Send a message when the form is submitted.
-    form.onsubmit = function(e) {
-      e.preventDefault();
-  
-      // Retrieve the message from the textarea.
-      var message = messageField.value;
-  
-      // Send the message through the WebSocket.
-      socket.send(message);
-  
-      // Clear out the message field.
-      messageField.value = '';
-  
-      return false;
-    };
-  
-  
-    // Close the WebSocket connection when the close button is clicked.
-    closeBtn.onclick = function(e) {
-      e.preventDefault();
-  
-      // Close the WebSocket.
-      socket.close();
-  
-      return false;
-    };
-  
-  };
-  
+//     var form = document.getElementById('message-form');
+//     var messageField = document.getElementById('message');
+//     var closeBtn = document.getElementById('close');
+
+//     var socket = new WebSocket('ws://127.0.0.1:3001/ws');
+
+//     socket.onerror = function(error) {
+//       console.log('WebSocket Error: ' + error);
+//     };
+
+//     socket.onopen = function(event) {
+//         console.log('open');
+//     };
+
+//     socket.onmessage = function(event) {
+//         console.log(event.data);
+//     };
+
+//     socket.onclose = function(event) {
+//         console.log('open');
+//     };
+
+//     form.onsubmit = function(e) {
+//       e.preventDefault();
+
+//       var message = messageField.value;
+
+//       socket.send(message);
+
+//       messageField.value = '';
+
+//       return false;
+//     };
+
+//     closeBtn.onclick = function(e) {
+
+//       e.preventDefault();
+//       socket.close();
+
+//       return false;
+//     };
+
+//   };
+
+
+function new_message(type, msg) {
+  //types: self, outside, self_reply, outside reply
+  let messageField = document.querySelector(".ui-msg-history");
+  let bubble = document.createElement("div");
+  switch (type) {
+    case "self":
+      console.log(type, msg);
+      bubble.classList.add("ui-bubble");
+      bubble.classList.add("self");
+      bubble.textContent = msg;
+      messageField.appendChild(bubble);
+      break;
+    case "outside":
+      console.log(type, msg);
+      bubble.classList.add("ui-bubble");
+      bubble.classList.add("outside");
+      bubble.textContent = msg;
+      messageField.appendChild(bubble);
+      break;
+    case "self_reply":
+      console.log(type, msg);
+      break;
+    case "outside_reply":
+      console.log(type, msg);
+      break;
+  }
+} 
+function testmsg() {
+  for (let i = 0; i < 159; i++) {
+  new_message("self", `hello${i}`);
+  new_message("outside", "hi!");
+}
+}
+
+function sort() {
+  let numbers = Array.from({ length: 100 }, (_, index) => index + 1);
+  for (let i = numbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+}
+for (let i = 0; i < numbers.length; i++) {
+  numbers.unshift(i);
+}
+  console.log(numbers);
+}
